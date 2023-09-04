59be99edcb939eb9d77b77cedc71da231dc4596e
Omal <omal@ascentic.se>
2023-09-04 12:58:03 +0530

    session 3 content

--- src/features/chat/actions/chatActions.ts
+++ src/features/chat/actions/chatActions.ts
@@ -1,17 +1,9 @@
-import sendMsgService from '../api/chatService';
-import {isLoading, updateThread} from '../reducer/chatReducer';
+import {updateThread} from '../reducer/chatReducer';
 import {ConversationType} from '../../../types/chat.types';
 import {Dispatch} from 'redux';
 
 export const sendMsgAction = (message: ConversationType) => {
   return (dispatch: Dispatch) => {
     dispatch(updateThread(message));
-    dispatch(isLoading(true));
-    sendMsgService(message.msgContent)
-      .then(response => {
-        dispatch(updateThread(response));
-        dispatch(isLoading(false));
-      })
-      .catch(e => console.log('error: ', e));
   };
 };
